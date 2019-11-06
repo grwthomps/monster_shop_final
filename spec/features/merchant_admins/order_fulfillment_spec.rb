9d1c39557ef19837a0ac12a1ae9e87d43cf1bfc1
Graham Thompson <grwthomps@gmail.com>
2019-11-05 18:59:41 -0700

    Refactor merchant order fulfillment tests by creating address for user and adding address_id to order

--- spec/features/merchant_admins/order_fulfillment_spec.rb
+++ spec/features/merchant_admins/order_fulfillment_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'When I visit an order show page as a merchant admin' do
   before :each do
     @user = User.create!(name: "Andy Dwyer", email: "test@gmail.com", password: "password123", password_confirmation: "password123")
+    @address = @user.addresses.create!(nickname: 'Home', street: '615 Oakwood Rd', city: 'Hudson', state: 'NH', zip: '03051')
 
     @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd', city: 'Denver', state: 'CO', zip: 80203)
     @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd', city: 'Denver', state: 'CO', zip: 80203)
@@ -10,7 +11,7 @@ RSpec.describe 'When I visit an order show page as a merchant admin' do
     @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 3)
     @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
 
-    @order = Order.create!(user_id: @user.id)
+    @order = Order.create!(user_id: @user.id, address_id: @address.id)
     @order.item_orders.create!(item_id: @tire.id, price: @tire.price, quantity: 7)
     @order.item_orders.create!(item_id: @pencil.id, price: @pencil.price, quantity: 104)
     @order.item_orders.create!(item_id: @paper.id, price: @paper.price, quantity: 3)
@@ -32,7 +33,10 @@ RSpec.describe 'When I visit an order show page as a merchant admin' do
 
     within '#customer-info' do
       expect(page).to have_content('Andy Dwyer')
-      expect(page).to have_content("123 Lincoln St Denver, CO 23840")
+      expect(page).to have_content('615 Oakwood Rd')
+      expect(page).to have_content('Hudson')
+      expect(page).to have_content('NH')
+      expect(page).to have_content('03051')
     end
 
     within "#item-#{@paper.id}" do
