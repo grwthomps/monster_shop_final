e9ee40e3c5658507c8730249d46b6743d55f77da
Graham Thompson <grwthomps@gmail.com>
2019-11-05 19:10:40 -0700

    Create model test for address no_orders? method

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -16,4 +16,17 @@ RSpec.describe Address, type: :model do
     it {should belong_to :user}
     it {should have_many :orders}
   end
+
+  describe 'instance methods' do
+    it 'can check for shipped orders with current address' do
+      user = User.create!(name: "Andy Dwyer", email: "test@gmail.com", password: "password123", password_confirmation: "password123")
+      address = user.addresses.create!(nickname: 'Work', street: "478 Hanover Blvd", city: "Denver", state: "CO", zip: 80128)
+
+      expect(address.no_orders?).to eq(true)
+
+      order = Order.create!(user_id: user.id, address_id: address.id, status: 2)
+
+      expect(address.no_orders?).to eq(false)
+    end
+  end
 end
